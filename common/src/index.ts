3eea93759a6d7a98642dcf5e993afb5336a56c59
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-04-21 17:23:55 +0100

    building common code package for npm

---
+++ common/src/index.ts
@@ -0,0 +1,15 @@
+interface Colour {
+    red: number;
+    blue: number;
+    green: number;
+}
+
+const colour: Colour = {
+    red: 20,
+    blue: 10,
+    green: 10
+};
+
+console.log(colour);
+
+export default colour;
