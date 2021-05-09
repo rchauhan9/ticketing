f7211bac2fe81f946cec473d95261506ed95dafc
Rajan <rajan.chauhan18@imperial.ac.uk>
2021-05-09 12:43:07 +0100

    Added ability to create record that payment has been made

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -4,6 +4,7 @@ import {requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthori
 import { Order } from "../models/order";
 import {OrderStatus} from "@rc-tickets/common";
 import { stripe } from "../stripe";
+import { Payment } from "../models/payment";
 
 const router = express.Router();
 
@@ -25,11 +26,17 @@ router.post('/api/payments', requireAuth, [body('token').not().isEmpty(), body('
         throw new BadRequestError("Cannot pay for a cancelled order");
     }
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
         currency: 'usd',
         amount: order.price * 100,
         source: token
     });
+
+    const payment = Payment.build({
+        orderId: orderId,
+        stripeId: charge.id
+    });
+    await payment.save();
 
     res.status(201).send({success: true});
 
